c721b6d4fa6f732342eda30f4324812dddf7d1f5
UK24424 <kaleem.ullah@cancom.at>
2024-07-31 12:33:08 +0200

    Update testScript1.ps1

--- testScript1.ps1
+++ testScript1.ps1
@@ -1,11 +1,27 @@
-$cars = @('Bmw', 'Honda', 'Mercedes', 'Suzuki') 
+$testResult = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell/issues?per_page=1&page=0&state=closed" -Method Get
+$issues = Invoke-RestMethod -Uri "https://api.github.com/repos/PowerShell/PowerShell/issues?per_page=100&page=0&state=closed" -Method Get
 
-foreach($car in $cars){
-    $car
-}
-echo ""
-echo ""
-$cars | foreach {$_.Length}
+# foreach ($testResult in $testResults) {
+#     $users = $testResult.user
+#     foreach ($user in $users) {
+#         $line = $user.id
+#         Write-Host $line
+#     }
+# }
+#$testResults | Get-Member
 
-#$cars | Get-Member
+#New-TimeSpan -Start $testResult.created_at -End $testResult.closed_at
 
+#$issues | Get-Member
+
+#(New-TimeSpan -Start $testResult.created_at -End $testResult.closed_at).TotalDays
+
+#$save = ($issues).count
+
+#echo "count $save"
+
+write-host "Average TTR for each issue: " -ForegroundColor Red ($issues | 
+    Select-Object @{n = 'TimeToResolve'; e = { (New-TimeSpan -Start $_.created_at -End $_.closed_at).TotalDays } } | 
+    Measure-Object -Property TimeToResolve -Average).Average
+
+    
